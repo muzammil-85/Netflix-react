59e4807f72484de8da0f35ef76ec776e6165f478
muzammil-85 <muzukondeth85@gmail.com>
2022-07-24 23:46:13 +0530

    first day

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css';
+import Banner from "./Components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
+
+// rfce
